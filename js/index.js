ce6512ddb83332ad8e374831ee49dce267f4f932
ZiaSifat <ziasifat150@gmail.com>
2024-07-09 23:53:12 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+function loadPhones(){
+    fetch(' https://openapi.programming-hero.com/api/phones?search=iphone')
+    .then(res=>res.json())
+    .then(data => displayData(data))
+}
+
+function displayData(phones){
+    const phoneContainer = document.getElementById('phone-container');
+    for(const phone of phones.data){
+        console.log(phone);
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card bg-gray-100 w-96 shadow-xl`;
+        phoneCard.innerHTML = `
+        <figure>
+        <img
+        src="${phone.image}"
+        alt="Shoes" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+        `;
+        phoneContainer.appendChild(phoneCard);
+        
+    }
+}
+
+loadPhones();
